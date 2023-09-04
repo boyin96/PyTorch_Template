8a7b91c81775edf011e26019ed7c0f7f748e3846
Bo Yin <boyin_scut@outlook.com>
2023-09-04 22:22:47 +0200

    Update train.py

--- train.py
+++ train.py
@@ -26,6 +26,7 @@ SEED = 123
 random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
 torch.cuda.manual_seed_all(SEED)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
